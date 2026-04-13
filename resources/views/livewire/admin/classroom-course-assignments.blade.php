92308305be66dcf20bc292a87f2cc16c07b49052
mldgato <manueldardon@hotmail.com>
2026-04-13 09:00:22 -0600

    Actualización de asignación de profesores para cambiar a un profesor aún cuando ya hay cuadros creados.

--- resources/views/livewire/admin/classroom-course-assignments.blade.php
+++ resources/views/livewire/admin/classroom-course-assignments.blade.php
@@ -213,7 +213,7 @@
                     </select>
                     <div class="input-group input-group-sm" style="width: 250px;">
                         <input type="text" wire:model.live.debounce.300ms="search" class="form-control"
-                            placeholder="Buscar aula..." autocomplete="new-password">
+                            placeholder="Buscar aula..." autocomplete="off">
                         <div class="input-group-append">
                             <button type="button" class="btn btn-default">
                                 <i class="fas fa-search"></i>
